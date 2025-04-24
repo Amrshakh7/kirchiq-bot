2ec8eae0ff179c651805197ff4190471de66c377
Amrshakh7 <anematov608@gmail.com>
2025-04-24 15:30:31 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.types import Message
+from aiogram.fsm.storage.memory import MemoryStorage
+
+API_TOKEN = '7583145649:AAFHwBXLVNs7AMFq3r59gMTAm45ANUtfpf0'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(storage=MemoryStorage())
+
+@dp.message(F.new_chat_members)
+async def new_member_handler(message: Message):
+    await message.delete()
+
+@dp.message(F.left_chat_member)
+async def left_member_handler(message: Message):
+    await message.delete()
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
